422080beb98ffedcc3cc6614f1f569ec3738f8f3
MahrokhJozedaemi2016 <mj485@njit.edu>
2024-09-24 13:50:56 -0500

    Updated calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,13 +1,17 @@
-"""My Calculator Test"""
+from my_calculator import MyCalculator
 
-from calculator import add, subtract
+def test_add():
+    """Test addition functionality."""
+    assert MyCalculator.add(2, 3) == 5
 
+def test_subtract():
+    """Test subtraction functionality."""
+    assert MyCalculator.subtract(5, 2) == 3
 
-def test_addition():
-    """Test that addiotn function works"""
-    assert add(2,2) == 4
+def test_multiply():
+    """Test multiplication functionality."""
+    assert MyCalculator.multiply(3, 3) == 9
 
-
-def test_subtraction():
-    """Test that addition function works"""
-    assert subtract(2,2) == 0
+def test_divide():
+    """Test division functionality."""
+    assert MyCalculator.divide(10, 2) == 5
